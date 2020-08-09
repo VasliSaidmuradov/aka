d65e9948bd4ee7ad53d80f6317b9f3c58306ed0f
Vasli Saidmuradov <vasli.said@gmail.com>
2020-08-10 00:14:42 +0600

    fix: meta title & description

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,11 @@ module.exports = {
   },
 
   head: {
-    title: 'aka',
+    title: 'AKA | Dental laboratory',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'AKA | Dental laboratory' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
